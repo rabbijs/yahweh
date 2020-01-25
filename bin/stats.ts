02115041b9ce446f5e73ac64b95880592140a6b9
Steven Zeiler <me@stevenzeiler.com>
2020-01-25 13:16:05 -0500

    get system cpu, ram, disk from stats

---
+++ bin/stats.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env ts-node
+
+const si = require('systeminformation');
+
+// promises style - new since version 3
+si.cpu()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.mem()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+
+si.fsSize()
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
